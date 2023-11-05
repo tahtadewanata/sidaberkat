4501b96b17ccde29d54003ab18389687fcda6c01
Tahta <tahtadewanata@gmail.com>
2023-11-05 19:54:26 +0700

    show leaflet

--- application/views/users/layout/sidebar.php
+++ application/views/users/layout/sidebar.php
@@ -21,7 +21,7 @@
   <link rel="apple-touch-icon" sizes="76x76" href="<?= base_url();?>soft-ui/assets/img/apple-icon.png">
   <link rel="icon" type="image/png" href="<?= base_url();?>assets/img/log.png">
   <title>
-    Dinas Arpus | Kab. Nganjuk
+    SIDABERKAT | Kab. Nganjuk
   </title>
   <!--     Fonts and icons     -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,89 +1,90 @@
+<?php $this->load->view('visitors/layout/header'); ?>
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-?><!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="utf-8">
-	<title>Welcome to CodeIgniter</title>
+defined('BASEPATH') or exit('No direct script access allowed');
+?>
+<!-- Content -->
+<div class="container-fluid">
+	<h4 class="py-3 mb-4"><span class="text-muted fw-light">Maps /</span> Leaflet</h4>
 
-	<style type="text/css">
+	<div class="row">
+		<!-- Basic -->
+		<div class="col-12">
+			<div class="card mb-4">
+				<h5 class="card-header">Basic Map</h5>
+				<div class="card-body">
+					<div class="leaflet-map" id="basicMap"></div>
+				</div>
+			</div>
+		</div>
+		<!-- /Basic -->
 
-	::selection { background-color: #E13300; color: white; }
-	::-moz-selection { background-color: #E13300; color: white; }
+		<!-- Marker Circle & Polygon -->
+		<div class="col-12">
+			<div class="card mb-4">
+				<h5 class="card-header">Marker Circle & Polygon</h5>
+				<div class="card-body">
+					<div class="leaflet-map" id="shapeMap"></div>
+				</div>
+			</div>
+		</div>
+		<!-- /Marker Circle & Polygon -->
 
-	body {
-		background-color: #fff;
-		margin: 40px;
-		font: 13px/20px normal Helvetica, Arial, sans-serif;
-		color: #4F5155;
-	}
+		<!-- Draggable Marker With Popup -->
+		<div class="col-12">
+			<div class="card mb-4">
+				<h5 class="card-header">Draggable Marker With Popup</h5>
+				<div class="card-body">
+					<div class="leaflet-map" id="dragMap"></div>
+				</div>
+			</div>
+		</div>
+		<!-- /Draggable Marker With Popup -->
 
-	a {
-		color: #003399;
-		background-color: transparent;
-		font-weight: normal;
-	}
+		<!-- User Location -->
+		<div class="col-12">
+			<div class="card mb-4">
+				<h5 class="card-header">User Location</h5>
+				<div class="card-body">
+					<div class="leaflet-map" id="userLocation"></div>
+				</div>
+			</div>
+		</div>
+		<!-- /User Location -->
 
-	h1 {
-		color: #444;
-		background-color: transparent;
-		border-bottom: 1px solid #D0D0D0;
-		font-size: 19px;
-		font-weight: normal;
-		margin: 0 0 14px 0;
-		padding: 14px 15px 10px 15px;
-	}
+		<!-- Custom Icons -->
+		<div class="col-12">
+			<div class="card mb-4">
+				<h5 class="card-header">Custom Icons</h5>
+				<div class="card-body">
+					<div class="leaflet-map" id="customIcons"></div>
+				</div>
+			</div>
+		</div>
+		<!-- /Custom Icons -->
 
-	code {
-		font-family: Consolas, Monaco, Courier New, Courier, monospace;
-		font-size: 12px;
-		background-color: #f9f9f9;
-		border: 1px solid #D0D0D0;
-		color: #002166;
-		display: block;
-		margin: 14px 0 14px 0;
-		padding: 12px 10px 12px 10px;
-	}
+		<!-- GeoJson -->
+		<div class="col-12">
+			<div class="card mb-4">
+				<h5 class="card-header">GeoJson</h5>
+				<div class="card-body">
+					<div class="leaflet-map" id="geoJson"></div>
+				</div>
+			</div>
+		</div>
+		<!-- /GeoJson -->
 
-	#body {
-		margin: 0 15px 0 15px;
-	}
-
-	p.footer {
-		text-align: right;
-		font-size: 11px;
-		border-top: 1px solid #D0D0D0;
-		line-height: 32px;
-		padding: 0 10px 0 10px;
-		margin: 20px 0 0 0;
-	}
-
-	#container {
-		margin: 10px;
-		border: 1px solid #D0D0D0;
-		box-shadow: 0 0 8px #D0D0D0;
-	}
-	</style>
-</head>
-<body>
-
-<div id="container">
-	<h1>Welcome to CodeIgniter!</h1>
-
-	<div id="body">
-		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
-
-		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
-
-		<p>The corresponding controller for this page is found at:</p>
-		<code>application/controllers/Welcome.php</code>
-
-		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
+		<!-- Layer Control -->
+		<div class="col-12">
+			<div class="card">
+				<h5 class="card-header">Layer Control</h5>
+				<div class="card-body">
+					<div class="leaflet-map" id="layerControl"></div>
+				</div>
+			</div>
+		</div>
+		<!-- /Layer Control -->
 	</div>
-
-	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
 </div>
+<!--/ Content -->
 
-</body>
-</html>
+<?php $this->load->view('visitors/layout/footer'); ?>
